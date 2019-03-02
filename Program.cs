0a15691a63920ffa8eb87c190b42f1546ff45fdc
MarkGelman <47790935+MarkGelman@users.noreply.github.com>
2019-03-02 13:23:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snova_Mnogotochie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Во введенной строке заменить первый символ "точка" (.) многоточием (...).
+
+            Начальные данные: строка текста.
+            Вывод результата: строка с заменой.
+
+            Полезные ссылки: Метод Substring(),     Метод IndexOf()*/
+
+            string line = Console.ReadLine();
+            if (line.Contains("."))
+                Console.WriteLine(line.Substring(0, line.IndexOf('.')) + ".." + line.Substring(line.IndexOf('.')));
+            else
+                Console.WriteLine(line);
+        }
+    }
+}
